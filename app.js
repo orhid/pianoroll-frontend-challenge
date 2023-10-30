0b54a9a541433dce247650db119d2111bc0bc7fb
Filip Øskar Łanecki <9296944+orhid@users.noreply.github.com>
2023-10-30 11:18:42 -0400

    simple selection, without counting notes

--- app.js
+++ app.js
@@ -1,20 +1,82 @@
 import PianoRoll from './pianoroll.js';
 
+// # useful elements
 const pianoRollSpotlight = document.getElementById('pianoRollSpotlight');
 const pianoRollGrid = document.getElementById('pianoRollGrid');
+let canvas;
+let context;
+
+// # globals
+let start = {};
+let selections = [];
+
+// # helper functions
 
 function putMeInSpotlight() {
   // remove previous piano roll from spotlight, if any
   while (pianoRollSpotlight.firstChild) {
     const child = pianoRollSpotlight.firstChild;
     child.addEventListener('click', putMeInSpotlight);
+    child.removeChild(document.getElementById('selections'));
     pianoRollSpotlight.removeChild(child);
     pianoRollGrid.appendChild(child);
   }
 
+  // apend to spotlight
   this.removeEventListener('click', putMeInSpotlight);
   pianoRollSpotlight.appendChild(this);
+
+  // create canvas for selection feature
+  canvas = document.createElement('canvas');
+  context = canvas.getContext('2d');
+
+  canvas.id = 'selections';
+  canvas.addEventListener('mouseup', endSelection);
+  canvas.addEventListener('mousedown', startSelection);
+  canvas.addEventListener('mousemove', drawMove);
+
+  this.appendChild(canvas);
 }
+
+// using 'evnt' since 'event' is a keyword
+function getMousePosition(canvas, evnt) { 
+  let selection = canvas.getBoundingClientRect();
+  return {
+    x: (evnt.clientX - selection.left) * canvas.width / selection.width,
+    y: (evnt.clientY - selection.top) * canvas.height / selection.height,
+  };
+}
+
+function startSelection(evnt) {
+  start = getMousePosition(canvas, evnt);
+}
+
+function endSelection(evnt) {
+  let { x, y } = getMousePosition(canvas, evnt);
+  selections.push({x: start.x, y: 0, width: x - start.x, height: canvas.height});
+  console.log(start.x, x-start.x); // print output
+  start = {};
+  draw();
+}
+
+function drawMove(evnt) {
+  if (start.x) {
+    draw()
+    let { x, y } = getMousePosition(canvas, evnt)
+    context.fillStyle = 'rgba(255, 192, 0, 0.72)';
+    context.fillRect(start.x, 0, x - start.x, canvas.height);
+  }
+}
+
+function draw() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  selections.forEach(rect => {
+    context.fillStyle = 'rgba(255, 192, 0, 0.54)';
+    context.fillRect(rect.x, rect.y, rect.width, rect.height);
+  }); 
+}
+
+// - - -
 
 class PianoRollDisplay {
   constructor(csvURL) {
@@ -81,3 +143,6 @@ document.getElementById('loadCSV').addEventListener('click', async function() {
   await csvToSVG.generateSVGs();
   this.style.display = 'none';
 });
+
+// draw()
+
